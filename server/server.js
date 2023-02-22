588288a4a846c5540b622f06e76186a6b91ba5fc
Kacper Tokaj <kacpertokajj@gmail.com>
2023-02-22 13:23:58 +0100

    starting server

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const http = require('http')
+
+
+
+app.listen(3002, function () {
+    console.log("started 3002")
+})
